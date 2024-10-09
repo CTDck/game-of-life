ad819bbce67f003e772d543c3fb084d532e17b0d
CTDck <camerondick17@gmail.com>
2024-10-09 20:15:45 +0100

    Use gameboard and randomly populate the board

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from game import GameBoard
+
+if __name__ == "__main__":
+    game = GameBoard(shape=(10, 20))
+    print(f"Game Board Shape {game.board.shape}")
+    game.populate_random(100)
+    print()
+    print(f"Randomly populated Board")
+    print(game.board)
+    print()
